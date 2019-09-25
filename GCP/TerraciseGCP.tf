62722852cf5a7be0b1e109c5403d531a955448b9
Riens Winoto <rienslw@outlook.com>
2019-09-25 16:47:49 +0700

    add google_compute_firewall and script adjustment

--- GCP/TerraciseGCP.tf
+++ GCP/TerraciseGCP.tf
@@ -7,19 +7,37 @@ provider "google" {
     zone        = "yourDesiredZone"
 }
 
+#setup variable
+variable "prefix" {
+    default = "terra"
+}
+
+variable "region" {
+    default = "asia-southeast1"
+}
+
+variable "zone" {
+    default = "asia-southeast1-a"
+}
+
+variable "tags" {
+    default = ["aks","devops"]
+}
+
 #setup for VPC network
-resource "google_compute_network" "vpc_network" {
-    name = "terraform-network"
+resource "google_compute_network" "terra_vpc_network" {
+    name = "${var.prefix}-network"
 }
 
 #setup for Compute Engine
-resource "google_compute_instance" "vm_instance" {
+resource "google_compute_instance" "terra_vm_instance" {
     depends_on   = [google_compute_address.terra_vm_ip, 
-                    google_compute_network.vpc_network]
+                    google_compute_network.terra_vpc_network]
 
-    name         = "terraform-instance"
+    name         = "${var.prefix}-instance"
+    zone         = "${var.zone}"
     machine_type = "f1-micro"
-    tags         = ["aks","devops"]
+    tags         = "${var.tags}"
 
     boot_disk {
         initialize_params {
@@ -28,25 +46,38 @@ resource "google_compute_instance" "vm_instance" {
     }
 
     network_interface {
-        network = google_compute_network.vpc_network.self_link
+        network = "${google_compute_network.terra_vpc_network.self_link}"
         
         access_config {
-            nat_ip = google_compute_address.terra_vm_ip.address
+            nat_ip = "${google_compute_address.terra_vm_ip.address}"
         }
     }
 }
 
 resource "google_compute_address" "terra_vm_ip" {
-    name = "terraform-static-ip"
+    name = "${var.prefix}-static-ip"
+}
+
+resource "google_compute_firewall" "terra_firewall" {
+    depends_on    = [google_compute_network.terra_vpc_network]
+
+    name          = "${var.prefix}-firewall"
+    network       = "${google_compute_network.terra_vpc_network.name}"
+    source_ranges = ["0.0.0.0/0"]
+
+    allow {
+        protocol = "tcp"
+        ports    = ["22","80","443"]
+    }
 }
 
 #setup for Kubernetes Engine
 resource "google_container_cluster" "terra_kube" {
-    depends_on         = [google_compute_network.vpc_network]
+    depends_on         = [google_compute_network.terra_vpc_network]
 
-    name               = "terra-cluster"
-    network            = google_compute_network.vpc_network.name
-    location           = "asia-southeast1"
+    name               = "${var.prefix}-cluster"
+    network            = "${google_compute_network.terra_vpc_network.name}"
+    location           = "${var.region}"
     initial_node_count = 1  
 
     addons_config {
@@ -68,9 +99,9 @@ resource "google_container_cluster" "terra_kube" {
 resource "google_container_node_pool" "terra_kube_nodes" {
     depends_on         = [google_container_cluster.terra_kube]
 
-    name               = "terrakube-node-pool"
-    location           = "asia-southeast1"
-    cluster            = google_container_cluster.terra_kube.name
+    name               = "${var.prefix}-node-pool"
+    location           = "${var.region}"
+    cluster            = "${google_container_cluster.terra_kube.name}"
     initial_node_count = 1
 
     management {
